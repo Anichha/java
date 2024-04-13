63ebcd1e40ac4d3660456a609529ca8909e0c7e7
Aniccha <shubhwaghmare27@gmail.com>
2024-04-13 13:27:51 +0530

    all added

---
+++ My_project1/src/main/java/com/project/CollectInfo.java
@@ -0,0 +1,10 @@
+package com.project;
+
+public class CollectInfo {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	System.out.println("hi");
+	}
+
+}
