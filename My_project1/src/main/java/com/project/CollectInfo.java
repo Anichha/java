24ad158ad0584a254e93050ba178f2e46f92bda7
Aniccha <shubhwaghmare27@gmail.com>
2024-04-14 11:54:57 +0530

    new way

--- My_project1/src/main/java/com/project/CollectInfo.java
+++ My_project1/src/main/java/com/project/CollectInfo.java
@@ -1,12 +1,41 @@
 package com.project;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class CollectInfo {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-	Class.forName("com.mysql.cj.jdbc.Driver");
-	InsertData.register();
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		switch (x) {
+		case 1: {
+			System.out.println("Enter Roll number");
+			int rollNum = sc.nextInt();
+
+			System.out.println("Enter Name");
+			String name = sc.next();
+
+			System.out.println("Enter Class  ");
+			String cls = sc.next();
+
+			System.out.println("Enter gender");
+			String gender = sc.next();
+
+			System.out.println("Enter Blood group");
+			String blood = sc.next();
+			InsertData in = new InsertData(rollNum, name, cls, gender, blood);
+			InsertData.register();
+		}
+		
+		case 2: 
+		{
+
+		}
+		
+		}
+
 	}
 
 }
--- My_project1/src/main/java/com/project/InsertData.java
+++ My_project1/src/main/java/com/project/InsertData.java
@@ -4,34 +4,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Scanner;
+
 
 public class InsertData {
-public static void register() throws SQLException
+static int rollNum;
+ static String name;
+ static String cls;
+ static String gender;
+  static String blood;
+ InsertData(int rollNum,String name,String cls,String gender,String blood)
+ {
+	 this.rollNum=rollNum;
+	 this.name=name;
+	 this.cls=cls;
+	 this.gender=gender;
+	 this.blood=blood;
+ }
+public static void register( ) throws SQLException
 {
-	Scanner sc= new Scanner(System.in);
-	System.out.println("Enter Roll number");
-	int rollNum= sc.nextInt();
 	
-	System.out.println("Enter Name");
-	String Name= sc.next();
-	
-	System.out.println("Enter Class  ");
-	String cls= sc.next();
-	
-	System.out.println("Enter gender");
-	String Gender= sc.next();
-	
-
-	System.out.println("Enter Blood group");
-	String blood= sc.next();
 	
 	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Students","root","root");
 	PreparedStatement ps=con.prepareStatement("insert into Students value(?,?,?,?,?)");
 	ps.setInt(1, rollNum);
-	ps.setString(2, Name);
+	ps.setString(2, name);
 	ps.setString(3, cls);
-	ps.setString(4, Gender);
+	ps.setString(4, gender);
 	ps.setString(5, blood);
 	int i=ps.executeUpdate();
 	if(i>0)System.out.println("Registration complete!");
